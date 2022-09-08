e8d227ee41fd9ab167cb63e20a3fa70268fa912a
Diego <diegoccpinto@gmail.com>
2022-09-08 11:17:43 -0300

    Exercicios 9 pronto

--- index.js
+++ index.js
@@ -231,13 +231,33 @@
 // Resultado
 // Apresente a mensagem "A=" seguida do valor da variável, como no exemplo abaixo, com quatro casas após a vírgula. Use todas as variáveis ​​de precisão dupla. Como todos os problemas, não se esqueça de imprimir o final de linha após o resultado, caso contrário você receberá "Erro de apresentação".
 
-function areaCircunf(r) {
-  let pi = 3.14159;
-  let valorLimite = (pi * r ** 2).toFixed(4);
+// function areaCircunf(r) {
+//   let pi = 3.14159;
+//   let valorLimite = (pi * r ** 2).toFixed(4);
 
-  return console.log('A = ' + valorLimite);
+//   return console.log('A = ' + valorLimite);
+// }
+
+// areaCircunf(2.0);
+// areaCircunf(100.64);
+// areaCircunf(150.0);
+
+// - Exercicio 9:
+
+// Leia quatro valores inteiros chamados A, B, C e D. Calcule e imprima a diferença do produto A e B pelo produto de C e D (A * B - C * D).
+
+// **Entrada**
+// O arquivo de entrada contém 4 valores inteiros.
+
+// **Resultado**
+// Imprima DIFERENCA com todas as letras maiúsculas, conforme exemplo a seguir, com um espaço em branco antes e depois do sinal de igual.
+
+function valueProduct(a, b, c, d) {
+  let multProd = a * b - c * d;
+
+  return console.log('DIFERENÇA: ' + multProd);
 }
 
-areaCircunf(2.0);
-areaCircunf(100.64);
-areaCircunf(150.0);
+valueProduct(5, 6, 7, 8);
+valueProduct(0, 0, 7, 8);
+valueProduct(5, 6, -7, 8);
